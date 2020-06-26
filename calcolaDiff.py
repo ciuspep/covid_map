6d0bd9f6192d9eab1d3d81ddd2000502ec76838d
ciuspep <ciusdev@gmail.com>
2020-06-26 14:17:28 +0200

    filtrati dati province inconsistenti

--- calcolaDiff.py
+++ calcolaDiff.py
@@ -18,12 +18,13 @@ def diffData(aStringDay):
     covid2['diff'] = 0
 
     for index in covid2.index:
-        diff = covid2.loc[index, 'totale_casi'] - \
-            covid1.loc[index, 'totale_casi']
-        covid2.loc[index, 'diff'] = diff  # if diff > 0 else 0
-        if diff < 0:
-            print(covid2.loc[index, 'denominazione_provincia'],
-                  covid2.loc[index, 'diff'])
+        if index < 200:
+            diff = covid2.loc[index, 'totale_casi'] - \
+                covid1.loc[index, 'totale_casi']
+            covid2.loc[index, 'diff'] = diff  # if diff > 0 else 0
+            if diff < 0:
+                print(covid2.loc[index, 'denominazione_provincia'],
+                      covid2.loc[index, 'diff'])
 
     covid2.to_csv(base_dir + 'dpc-covid19-ita-province-diff.csv',
                   index=True, header=True)
